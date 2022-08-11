edf71f0e2afb23e9c5d5a248bc8fdb228c0d2b7a
NiKeSh-V <54545457+Nikesh6920@users.noreply.github.com>
2022-08-11 20:58:07 +0530

    first commit

---
+++ src/test/java/com/mart/freshmart/FreshmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mart.freshmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
